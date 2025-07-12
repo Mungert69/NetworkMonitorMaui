970d32c20cc921b90f823020b32d2b35384a7ef9
Mahadeva <contact@mahadeva.co.uk>
2025-07-12 02:52:02 +0100

    fixed openssl and oqsprovider

--- Helpers/CopyAssetsHelper.cs
+++ Helpers/CopyAssetsHelper.cs
@@ -264,7 +264,9 @@ private static async Task<string> CopyAssetType(string assetType, bool setPerms,
                 string ldLibraryPath = $"LD_LIBRARY_PATH={libraryPath}";
                 Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", libraryPath);
                 outputStr.AppendLine($"Set LD_LIBRARY_PATH to: {libraryPath}");
-            }
+           Environment.SetEnvironmentVariable("OPENSSL_MODULES", libraryPath);
+            outputStr.AppendLine($"Set OPENSSL_MODULES to: {libraryPath}");
+        }
             catch (Exception ex)
             {
                 outputStr.AppendLine($"Failed to set LD_LIBRARY_PATH: {ex.Message}");
