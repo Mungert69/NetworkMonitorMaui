57ca016cc5e62d8ce06c55c50b7e39fce3ade6ff
Mungert69 <contact@mahadeva.co.uk>
2025-08-21 13:30:48 +0100

    .

--- Services/AndroidBackgroundService.cs
+++ Services/AndroidBackgroundService.cs
@@ -2,6 +2,8 @@
 using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Graphics;
+using AndroidX.Core.App;
 using Microsoft.Extensions.Logging;
 using NetworkMonitor.Connection;
 using NetworkMonitor.Processor.Services;
@@ -11,7 +13,6 @@ using NetworkMonitor.Utils.Helpers;
 using NetworkMonitor.DTOs;
 using NetworkMonitor.Objects;
 using Microsoft.Extensions.Configuration;
-using AndroidX.Core.App;
 using NetworkMonitor.Maui.Services;
 using NetworkMonitor.Maui;
 
@@ -22,7 +23,7 @@ namespace NetworkMonitor.Maui.Services
     [Android.App.Service(ForegroundServiceType = global::Android.Content.PM.ForegroundService.TypeConnectedDevice)]
     public class AndroidBackgroundService : Android.App.Service
     {
-        private CancellationTokenSource _cts;
+        private CancellationTokenSource _cts = new();
         // This is any integer value unique to the application.
         public const int SERVICE_RUNNING_NOTIFICATION_ID = 10000;
         private ILogger _logger;
@@ -32,27 +33,33 @@ namespace NetworkMonitor.Maui.Services
         private IBackgroundService _backgroundService;
         private IMonitorPingInfoView _monitorPingInfoView;
         private LocalProcessorStates _processorStates;
-        //private ILocalCmdProcessorStates _scanProcessorStates;
         private ICmdProcessorProvider _cmdProcessorProvider;
-
         private IPlatformService _platformService;
-
         private IFileRepo _fileRepo;
         private IRootNamespaceProvider _rootProvider;
+        private NotificationManagerCompat _compatManager;
+        private int messageId = 0;
+        private string _channelName = "FreeNetworkMonitor";
+        private string _channelId = "fre_mon_channel";
+        private string _channelDescription = "Quantum Network Monitor Agent notification channel";
+        private bool _channelInitialized = false;
+        private ILaunchHelper _launchHelper;
+
         public const string ServiceBroadcastAction = "com.networkmonitor.service.STATUS";
         public const string ServiceStatusExtra = "ServiceStatus";
         public const string ServiceMessageExtra = "ServiceMessage";
 
+
         public AndroidBackgroundService()
         {
-            _rootProvider = ServiceInitializer.RootProvider;
-
+            _rootProvider = ServiceInitializer.RootProvider!;
         }
 
-        public override IBinder OnBind(Intent intent)
+        public override IBinder? OnBind(Intent? intent)
         {
             return null;
         }
+
         public override void OnCreate()
         {
             base.OnCreate();
@@ -67,14 +74,13 @@ namespace NetworkMonitor.Maui.Services
             _processorStates = _rootProvider.ServiceProvider.GetRequiredService<LocalProcessorStates>();
             _cmdProcessorProvider = _rootProvider.ServiceProvider.GetRequiredService<ICmdProcessorProvider>();
             _platformService = _rootProvider.ServiceProvider.GetRequiredService<IPlatformService>();
-            _backgroundService = new BackgroundService(_logger, _netConfig, _loggerFactory, _rabbitRepo, _fileRepo, _processorStates, _monitorPingInfoView, _cmdProcessorProvider);
-            
+            _launchHelper = _rootProvider.ServiceProvider.GetRequiredService<ILaunchHelper>();
         }
         private async Task StartAsync()
         {
             try
             {
-                
+                _backgroundService = new BackgroundService(_logger, _netConfig, _loggerFactory, _rabbitRepo, _fileRepo, _processorStates, _monitorPingInfoView, _cmdProcessorProvider,_launchHelper);
                 var result = await _backgroundService.Start();
                 _platformService.OnUpdateServiceState(result, true);
 
@@ -87,6 +93,7 @@ namespace NetworkMonitor.Maui.Services
 
         private async Task StopAsync()
         {
+             if (_backgroundService == null) return;
             try
             {
                 var result = await _backgroundService.Stop();
@@ -102,32 +109,36 @@ namespace NetworkMonitor.Maui.Services
         {
             var viewAppIntent = new Intent(this, _rootProvider.MainActivity);
             viewAppIntent.AddCategory(Intent.CategoryLauncher);
-            return PendingIntent.GetActivity(this, 0, viewAppIntent, 0);
+            // PendingIntent.GetActivity can return null, so check and throw if needed
+            var pendingIntent = PendingIntent.GetActivity(this, 0, viewAppIntent, 0);
+            if (pendingIntent == null)
+                throw new InvalidOperationException("Failed to create PendingIntent.");
+            return pendingIntent;
         }
 
-        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        public override StartCommandResult OnStartCommand(Intent? intent, StartCommandFlags flags, int startId)
         {
             if (_cts.IsCancellationRequested)
             {
                 _cts = new CancellationTokenSource();
             }
-           
-            if (intent.Action == "STOP_SERVICE")
-                {
+            string action = intent?.Action;
+            if (action == "STOP_SERVICE")
+            {
                 try
                 {
                     _logger.LogInformation($" SERVICE : stopping");
-
-                    Task.Run(async () =>
-                        {
-#pragma warning disable CA1422
-                            StopForeground(true);
-                            //StopBackgroundService(true);
-#pragma warning restore CA1422
-                            await StopAsync();
-                        }, _cts.Token);
+                    if ((int)Build.VERSION.SdkInt >= 24)
+                    {
+                        StopForeground(Android.App.StopForegroundFlags.Remove);
+                    }
+                    else
+                    {
+                        StopForeground(true);
+                    }
+                    _ = StopAsync();
                     _logger.LogInformation($" SERVICE : StartCommand Stop Completed");
-            
+
                     return StartCommandResult.Sticky;
                 }
                 catch (Exception e)
@@ -137,80 +148,71 @@ namespace NetworkMonitor.Maui.Services
                     return StartCommandResult.Sticky;
                 }
             }
-           
+
             try
             {
                 int logoId = _rootProvider.GetDrawable("logo");
                 int viewId = _rootProvider.GetDrawable("view");
                 _logger.LogInformation($" SERVICE : drawables {logoId} : {viewId}");
-                   if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-                   {
-#pragma warning disable CA1416, CA1422
-                       Notification notification;
-                       NotificationChannel channel = new NotificationChannel("channel_id", "Free Network Monitor Agent", NotificationImportance.Low);
-                       NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
-                       notificationManager.CreateNotificationChannel(channel);
-                       /*var stopAction = new Notification.Action.Builder(
-                                _rootProvider.GetDrawable(".stop,
-                               "Stop",
-                               GetStopServicePendingIntent())
-                                .Build();
+                if (!_channelInitialized)
+                {
+                    CreateNotificationChannel();
+                }
 
-                       var viewAction = new Notification.Action.Builder(
-                             _rootProvider.GetDrawable(".view,
-                            "View",
-                            GetViewAppPendingIntent())
-                           .Build();
-                           */
-                       _logger.LogInformation($" SERVICE : created notification channel.");
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, _channelId)
+                    .SetContentTitle("Network Monitor Agent")
+                    .SetContentText("Service Running...")
+                    .SetLargeIcon(BitmapFactory.DecodeResource(Platform.AppContext.Resources, logoId))
+                    .SetSmallIcon(logoId)
+                    .SetOngoing(true);
 
-                       notification = new Notification.Builder(this, "channel_id")
-                           .SetAutoCancel(false)
-                           .SetOngoing(true)
-                           .SetContentTitle("Free Network Monitor Agent")
-                           .SetContentText("Monitoring network...")
-                           .SetSmallIcon( _rootProvider.GetDrawable("logo"))
-                           .Build();
+                Notification notification = builder.Build();
+                _logger.LogInformation($" SERVICE : created notification");
 
-                       _logger.LogInformation($" SERVICE : cratetd notification");
+                // Only call StartForeground with ForegroundService.TypeConnectedDevice if API >= 29
+                if ((int)Build.VERSION.SdkInt >= 29)
+                {
+                    StartForeground(SERVICE_RUNNING_NOTIFICATION_ID, notification,
+                        Android.Content.PM.ForegroundService.TypeConnectedDevice);
+                }
+                else
+                {
+                    StartForeground(SERVICE_RUNNING_NOTIFICATION_ID, notification);
+                }
 
-                       if (Build.VERSION.SdkInt < BuildVersionCodes.Tiramisu)
-                       {
-                           StartForeground(SERVICE_RUNNING_NOTIFICATION_ID, notification);
-                       }
-                       else
-                       {
-                           StartForeground(SERVICE_RUNNING_NOTIFICATION_ID, notification,
-                            Android.Content.PM.ForegroundService.TypeConnectedDevice);
-                       }
-#pragma warning restore CA1416, CA1422
-                   }
-                   else
-                   {
-#pragma warning disable CS0618
-                       // For API below 26
-                       Notification notification = new NotificationCompat.Builder(this)
-                                       .SetContentTitle("Free Network Monitor Agent")
-                                       .SetContentText("Monitoring network...")
-                                       .SetSmallIcon( _rootProvider.GetDrawable("logo"))
-                                       .SetOngoing(true)
-                                       //.AddAction( _rootProvider.GetDrawable(".stop, "Stop", GetStopServicePendingIntent())
-                                       .AddAction( _rootProvider.GetDrawable("view"), "Open", GetViewAppPendingIntent()) // Ensure you have an icon for 'View App'
-                                       .Build();
-                       StartForeground(SERVICE_RUNNING_NOTIFICATION_ID, notification);
-#pragma warning restore CS0618
-                   }
-                    StartAsync();
-                   
+                _ = StartAsync();
+
             }
             catch (Exception e)
             {
                 var result = new ResultObj() { Message = $" Error : Failed to Start service . Error was : {e.Message}", Success = false };
+                result.Success = false;
                 _platformService.OnUpdateServiceState(result, true);
             }
             _logger.LogInformation($" SERVICE : StartCommand Start completed");
+
             return StartCommandResult.Sticky;
         }
+
+private void CreateNotificationChannel()
+{
+    // Only on API 26+ (Oreo)
+    if ((int)Build.VERSION.SdkInt >= 26)
+    {
+        var channelNameJava = new Java.Lang.String(_channelName);
+        var channel = new NotificationChannel(_channelId, channelNameJava, NotificationImportance.Default)
+        {
+            Description = _channelDescription
+        };
+        var managerObj = Platform.AppContext.GetSystemService(Context.NotificationService);
+        if (managerObj is NotificationManager manager)
+        {
+            manager.CreateNotificationChannel(channel);
+            _channelInitialized = true;
+            _logger.LogInformation($" SERVICE : created notification channel.");
+        }
+    }
+}
 
         public override void OnDestroy()
         {
@@ -219,7 +221,7 @@ namespace NetworkMonitor.Maui.Services
                 Task.Run(async () =>
                 {
                     await StopAsync();
-                }).Wait(TimeSpan.FromSeconds(10)); // Give it 5 seconds to complete
+                }).Wait(TimeSpan.FromSeconds(5)); // Give it 5 seconds to complete
             }
             catch (Exception e)
             {
